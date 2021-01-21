8340f3c6ae7489a0f5c5c5e2d06b94f05751fa7d
Antony Nifosi <nifosi.antony@gmail.com>
2021-01-21 11:44:45 +0100

    Fix d'un petit bug

--- sudoku.cpp
+++ sudoku.cpp
@@ -42,7 +42,7 @@ bool absentSurColonne (int k, std::array<std::array<int , 9>, 9>& grille, int j)
 
 bool absentSurBloc (int k, std::array<std::array<int , 9>, 9>& grille, int i, int j)
 {
-    int _i = i-(i%3), _j = j-(j%3);  // ou encore : _i = 3*(i/3), _j = 3*(j/3);
+    int _i = i-(i%3), _j = j-(j%3);
     for (i=_i; i < _i+3; i++)
         for (j=_j; j < _j+3; j++)
             if (grille[i][j] == k)
